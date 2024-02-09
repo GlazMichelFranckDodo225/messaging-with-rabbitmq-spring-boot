0454cf9a8910e56e74e32d108bebf56dca3b4a59
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-02-09 03:44:14 +0100

    Create a RabbitMQ Message Receiver - V2

--- src/main/java/com/dgmf/messaging/Receiver.java
+++ src/main/java/com/dgmf/messaging/Receiver.java
@@ -8,11 +8,15 @@ import java.util.concurrent.CountDownLatch;
 public class Receiver {
     private CountDownLatch latch = new CountDownLatch(1);
 
+    // Method for Receiving Messages
     public void receiveMessage(String message) {
         System.out.println("Receive <" + message + ">");
+
+        // To Signal that the Message Has Been Received. Not to
+        // Implement in Production
         latch.countDown();
     }
-        
+
     public CountDownLatch getLatch() {
         return latch;
     }
